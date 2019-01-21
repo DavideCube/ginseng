1c988374b52c9f9843f836eff21b75e2b57ec661
the-radiator <34714232+davide-perez@users.noreply.github.com>
2019-01-21 15:12:28 +0100

    Optimized pow function

--- functions.h
+++ functions.h
@@ -1,7 +1,20 @@
-float myPow(float p, int exponent){
-	int res = 1;
-	for(int i = 0; i < exponent; i++)
-		res *= p; 
-		
-	return res;
+/**
+* Power function implemented with exp by squaring (shift in binaries).
+* Runs in log2(n), where n is the exponent.
+*/
+float myPow(float base, int exp){
+
+	int result = 1;
+	for (;;){
+
+        if (exp & 1)
+            result *= base;
+        exp >>= 1;
+        if (!exp)
+            break;
+        base *= base;
+    }
+
+    return result;
+
 }
